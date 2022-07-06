fc82b3ebe75d2ff1a45d19a401528b113938bbeb
esha777 <84219082+esha777@users.noreply.github.com>
2022-07-06 12:49:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+objeets = [];
+status = "";
+
+function setup() {
+    canvas= createCanvas(380,  380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!")
+    status = true;
+}
+
+function start()
+ {
+objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("status").innerHTML = "Status : Detecting Objects";
+objet_name = document.getElementById("objet_name").value;    
+}
+
+function draw() {
+   image(video, 0, 0, 380, 380);
+}
